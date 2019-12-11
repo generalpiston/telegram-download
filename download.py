3e35d4f0e422052429199598e6b7e3fe43aee3f2
Abraham Elmahrek <abraham@elmahrek.com>
2019-12-11 15:06:57 -0800

    telegram download

---
+++ download.py
@@ -0,0 +1,91 @@
+import asyncio
+import configparser
+import os
+from telethon import TelegramClient
+
+CONFIG_INI = 'telegram-download.ini'
+
+class TelegramDownload(object):
+  def __init__(self, api_id, api_hash, output, entity='me', delete_on_download=False):
+    self.api_id = api_id
+    self.api_hash = api_hash
+    self.entity = entity
+    self.output = output
+    self.delete_on_download = delete_on_download
+    self.client = TelegramClient('download', self.api_id, self.api_hash)
+    self.client.start()
+
+  def find_photos(self):
+    print('Finding photos for {}'.format(self.entity))
+
+    messages = []
+
+    for message in self.client.iter_messages(self.entity):
+      if hasattr(message, 'media') and hasattr(message.media, 'photo'):
+        messages.append(message)
+
+    return messages
+
+  def download_photos(self, messages=[]):
+    print('Downloading photos to {}'.format(self.output))
+
+    if not os.path.isdir(self.output):
+      os.mkdir(self.output)
+
+    loop = asyncio.get_event_loop()
+    tasks = []
+
+    for message in messages:
+      tasks.append(message.download_media(self.output))
+
+    loop.run_until_complete(asyncio.gather(*tasks))
+
+  def execute(self):
+    messages = self.find_photos()
+    self.download_photos(list(messages))
+    if self.delete_on_download:
+      self.delete_photos(messages)
+
+  def delete_photos(self, messages):
+    print('Deleting photos')
+
+    loop = asyncio.get_event_loop()
+    tasks = []
+
+    for message in messages:
+      tasks.append(message.delete())
+
+    loop.run_until_complete(asyncio.gather(*tasks))
+
+
+def write_config(path=CONFIG_INI):
+  config = configparser.ConfigParser()
+
+  print("Generate the answers to the following questions here: http://my.telegram.org/.")
+  config['DEFAULT']['api_id'] = input("API ID: ")
+  config['DEFAULT']['api_hash'] = input("API Hash: ")
+  config['DEFAULT']['output'] = input("Output Location: ")
+
+  with open(path, 'w') as f:
+    config.write(f)
+
+def read_config(path=CONFIG_INI):
+  config = configparser.ConfigParser()
+  config.read(path)
+  return config['DEFAULT']
+
+def ensure_config(path=CONFIG_INI):
+  if not os.path.isfile(path):
+    write_config(path)
+
+  return read_config()
+
+def main():
+  config = ensure_config()
+  delete_on_download = input("Delete Photos On Download? (Y|N) (Default: N): ") == 'Y'
+  entity = input("Channel? (Default: Saved Messages): ") or "me"
+
+  client = TelegramDownload(config['api_id'], config['api_hash'], config['output'], entity, delete_on_download)
+  client.execute()
+
+main()
